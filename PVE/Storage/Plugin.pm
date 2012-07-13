1270370d9b79dafe5fdbe834058ced1fc56f03bd
Alexandre Derumier <aderumier@odiso.com>
2012-07-13 15:22:12 +0200

    set shared=1 to sheepdog plugin
    Signed-off-by: Alexandre Derumier <aderumier@odiso.com>

--- PVE/Storage/Plugin.pm
+++ PVE/Storage/Plugin.pm
@@ -300,7 +300,7 @@ sub parse_config {
 	    $d->{content} = $def->{content}->[1] if !$d->{content};
 	}
 
-	if ($type eq 'iscsi' || $type eq 'nfs' || $type eq 'rbd' ) {
+	if ($type eq 'iscsi' || $type eq 'nfs' || $type eq 'rbd' || $type eq 'sheepdog' ) {
 	    $d->{shared} = 1;
 	}
     }
