62ebea1e56a6efc6a8a8e2b017d185d4f4f499b4
Alexandre Derumier <aderumier@odiso.com>
2013-02-14 17:27:33 +0100

    sheppdog : has_feature : copy
    Signed-off-by: Alexandre Derumier <aderumier@odiso.com>

--- PVE/Storage/SheepdogPlugin.pm
+++ PVE/Storage/SheepdogPlugin.pm
@@ -432,6 +432,7 @@ sub volume_has_feature {
    my $features = {
 	snapshot => { current => 1, snap => 1},
 	clone => { base => 1},
+	copy => { base => 1, current => 1, snap => 1},
     };
 
     my ($vtype, $name, $vmid, $basename, $basevmid, $isBase) =
