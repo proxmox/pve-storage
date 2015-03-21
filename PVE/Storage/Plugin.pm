d26e1891ffc8c1b7c17e32e4af01770c378b6287
Dietmar Maurer <dietmar@proxmox.com>
2015-03-21 11:20:18 +0100

    mark drbd as shared storage

--- PVE/Storage/Plugin.pm
+++ PVE/Storage/Plugin.pm
@@ -312,7 +312,7 @@ sub parse_config {
 	    $d->{content} = $def->{content}->[1] if !$d->{content};
 	}
 
-	if ($type eq 'iscsi' || $type eq 'nfs' || $type eq 'rbd' || $type eq 'sheepdog' || $type eq 'iscsidirect' || $type eq 'glusterfs' || $type eq 'zfs') {
+	if ($type eq 'iscsi' || $type eq 'nfs' || $type eq 'rbd' || $type eq 'sheepdog' || $type eq 'iscsidirect' || $type eq 'glusterfs' || $type eq 'zfs' || $type eq 'drbd') {
 	    $d->{shared} = 1;
 	}
     }
