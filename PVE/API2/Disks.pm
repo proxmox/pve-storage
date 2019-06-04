558d412d10dd9862f109ee0eb3781e1d7b241eff
Dominik Csapak <d.csapak@proxmox.com>
2019-06-04 12:35:21 +0200

    Disks API: allow also unused disks for db/wal
    since we will create a pv/vg/lv on it with nautilus
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- PVE/API2/Disks.pm
+++ PVE/API2/Disks.pm
@@ -137,7 +137,7 @@ __PACKAGE__->register_method ({
 	    my $entry = $disks->{$disk};
 	    if ($type eq 'journal_disks') {
 		next if $entry->{osdid} >= 0;
-		next if !$entry->{gpt} && $entry->{used} ne 'LVM';
+		next if !($entry->{gpt} || !$entry->{used} || $entry->{used} eq 'LVM');
 	    } elsif ($type eq 'unused') {
 		next if $entry->{used};
 	    } elsif ($type ne '') {
