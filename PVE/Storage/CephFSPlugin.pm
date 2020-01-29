proxmox/pve-storage

commit 54e0b0034bd6654c566cb4ae7d4a5953c48cd1ca

cephfs: tell systemd that mount mount requires network
As we mount this manually and thus systemd doesn't know about any
dependency for cephFS mounts, this got umounted only at the last
stage of shutdown, where network wasn't active anymore.

But, CephFS needs to be connected to an active MDS for a clean
unmount so without network this mount would delay shutdown for quite
a bit, until after some minutes systemd gave up and forced unmount.

So tell systemd that this mount requires network, which can be done
with the '_netdev'[0] mount option, that lucky for us can be also
passed to a mount call and isn't only available for fstab.

with this a mount gets, among others:
> Wants=network-online.target
> Before=umount.target remote-fs.target
> After=remote-fs-pre.target system.slice network.target network-online.target -.mount

Which does the trick for us.

[0]: https://www.freedesktop.org/software/systemd/man/systemd.mount.html#_netdev

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/Storage/CephFSPlugin.pm
@@ -66,6 +66,10 @@ sub cephfs_mount {
 	$cmd = ['/bin/mount', '-t', 'ceph', $source, $mountpoint, '-o', "name=$cmd_option->{userid}"];
 	push @$cmd, '-o', "secretfile=$secretfile" if defined($secretfile);
     }
+    # tell systemd that we're network dependent, else it umounts us to late on
+    # shutdown, when we couldn't connect to the active MDS and thus unmount
+    # hangs and delays shutdown/reboot (man systemd.mount)
+    push @$cmd, '-o', '_netdev';
 
     if ($scfg->{options}) {
 	push @$cmd, '-o', $scfg->{options};

commit d9ece228fbbbf0eb575c8a067a70ed7023078f84

fix random hangs on reboot with active CephFS mount ordering cycle
commit 54e0b0034bd6654c566cb4ae7d4a5953c48cd1ca introduced the
"_netdev" option, for PVE 5.3. The systemd generator then correctly
resolved that in the following resulting order-dependencies:
> Wants=network-online.target
> Before=umount.target remote-fs.target
> After=remote-fs-pre.target system.slice network.target network-online.target -.mount

This worked well and all were happy. With the current systemd in 6.0
we sometimes get the local-fs ones there generated too. This is a
fallout from a try to better handling nested mount hierachies, where
a .mount unit needs to be mounter or unmounted, before or after,
respectively, the parent mount was processed. It seems that sometime
that glitches and thus a "RequireMountFor=/mnt/pve" gets thrown in
and result sometimes in the local-fs order constraints being added.

The issue now is, that one must not have ordering depends to all,
local-fs, local-fs-pre, remote-fs, remote-fs-pre, as that gets you a
ordering cycle. Systemd tries to solve that cycle by randomly
dropping one constraint and retrying. By luck this is a not so
important unit, and all goes on well. Most of the time one isn't that
lucky and something important gets dropped, for example:

> Jan 24 18:43:05 prod1 systemd[1]: sysinit.target: Found ordering cycle on systemd-timesyncd.service/stop
> Jan 24 18:43:05 prod1 systemd[1]: sysinit.target: Found dependency on systemd-tmpfiles-setup.service/stop
> Jan 24 18:43:05 prod1 systemd[1]: sysinit.target: Found dependency on local-fs.target/stop
> Jan 24 18:43:05 prod1 systemd[1]: sysinit.target: Found dependency on mnt-pve-cephfs.mount/stop
> Jan 24 18:43:05 prod1 systemd[1]: sysinit.target: Found dependency on remote-fs-pre.target/stop
> Jan 24 18:43:05 prod1 systemd[1]: sysinit.target: Found dependency on rbdmap.service/stop
> Jan 24 18:43:05 prod1 systemd[1]: sysinit.target: Found dependency on sysinit.target/stop

PVE/Storage/CephFSPlugin.pm
@@ -7,7 +7,7 @@ use IO::File;
 use Net::IP;
 use File::Path;
 
-use PVE::Tools qw(run_command);
+use PVE::Tools qw(run_command file_set_contents);
 use PVE::ProcFSTools;
 use PVE::Storage::Plugin;
 use PVE::JSONSchema qw(get_standard_option);
@@ -37,6 +37,7 @@ sub cephfs_is_mounted {
     return undef;
 }
 
+
 # FIXME: duplicate of api/diskmanage one, move to common helper (pve-common's
 #        Tools or Systemd ?)
 sub systemd_escape {
@@ -53,6 +54,41 @@ sub systemd_escape {
     $val =~ s/\//-/g;
 
     return $val;
+}
+
+# FIXME: remove in PVE 7.0 where systemd is recent enough to not have those
+#        local-fs/remote-fs dependency cycles generated for _netdev mounts...
+sub systemd_netmount {
+    my ($where, $type, $what, $opts) = @_;
+
+# don't do default deps, systemd v241 generator produces ordering deps on both
+# local-fs(-pre) and remote-fs(-pre) targets if we use the required _netdev
+# option. Over thre corners this gets us an ordering cycle on shutdown, which
+# may make shutdown hang if the random cycle breaking hits the "wrong" unit to
+# delete.
+    my $unit =  <<"EOF";
+[Unit]
+Description=${where}
+DefaultDependencies=no
+Requires=system.slice
+Wants=network-online.target
+Before=umount.target remote-fs.target
+After=systemd-journald.socket system.slice network.target -.mount remote-fs-pre.target network-online.target
+Conflicts=umount.target
+
+[Mount]
+Where=${where}
+What=${what}
+Type=${type}
+Options=${opts}
+EOF
+
+    my $unit_fn = systemd_escape($where, 1) . ".mount";
+    my $unit_path = "/run/systemd/system/$unit_fn";
+
+    file_set_contents($unit_path, $unit);
+    run_command(['systemctl', 'start', $unit_fn], errmsg => "mount error");
+
 }
 
 sub cephfs_mount {
@@ -77,22 +113,20 @@ sub cephfs_mount {
 	    push @$cmd, '-r', $subdir if !($subdir =~ m|^/$|);
 	    push @$cmd, $mountpoint;
 	    push @$cmd, '--conf', $configfile if defined($configfile);
+
+	    if ($scfg->{options}) {
+		push @$cmd, '-o', $scfg->{options};
+	    }
+
+	    run_command($cmd, errmsg => "mount error");
     } else {
 	my $source = "$server:$subdir";
-	$cmd = ['/bin/mount', '-t', 'ceph', $source, $mountpoint, '-o', "name=$cmd_option->{userid}"];
-	push @$cmd, '-o', "secretfile=$secretfile" if defined($secretfile);
+	my @opts = ( "name=$cmd_option->{userid}" );
+	push @opts, "secretfile=$secretfile" if defined($secretfile);
+	push @opts, $scfg->{options} if $scfg->{options};
 
-	# tell systemd that we're network dependent, else it umounts us to late
-	# on shutdown, when we couldn't connect to the active MDS and thus
-	# unmount hangs and delays shutdown/reboot (man systemd.mount).
-	push @$cmd, '-o', '_netdev';
+	systemd_netmount($mountpoint, 'ceph', $source, join(',', @opts));
     }
-
-    if ($scfg->{options}) {
-	push @$cmd, '-o', $scfg->{options};
-    }
-
-    run_command($cmd, errmsg => "mount error");
 }
 
 # Configuration