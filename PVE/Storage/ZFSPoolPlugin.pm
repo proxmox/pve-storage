proxmox/pve-storage

commit 815df2dd08ac4c7295135262e60d64fbb57b8f5c

ZFS: mount subvols in activate_volume
Makes it possible to clone and start a container whose
ZFS subvols are not yet mounted for some reason. If a
subvol cannot be mounted, there's a better error now:
zfs error: cannot mount '/myzpool/subvol-103-disk-0': directory is not empty

Previously, cloning would quietly do an "empty" clone,
and startup would fail with:
mount_autodev: 1074 Permission denied - Failed to create "/dev" directory
lxc_setup: 3238 Failed to mount "/dev"
do_start: 1224 Failed to setup container "103"
__sync_wait: 41 An error occurred in another process (expected sequence number 5)

Signed-off-by: Fabian Ebner <f.ebner@proxmox.com>

PVE/Storage/ZFSPoolPlugin.pm
@@ -549,9 +549,14 @@ sub activate_volume {
 
     my (undef, undef, undef, undef, undef, undef, $format) = $class->parse_volname($volname);
 
-    return 1 if $format ne 'raw';
-
-    $class->zfs_wait_for_zvol_link($scfg, $volname);
+    if ($format eq 'raw') {
+	$class->zfs_wait_for_zvol_link($scfg, $volname);
+    } elsif ($format eq 'subvol') {
+	my $mounted = $class->zfs_get_properties($scfg, 'mounted', "$scfg->{pool}/$volname");
+	if ($mounted !~ m/^yes$/) {
+	    $class->zfs_request($scfg, undef, 'mount', "$scfg->{pool}/$volname");
+	}
+    }
 
     return 1;
 }

commit 59fdc2b71e5da8eaf3c821a5c55f410f58da200a

fix regression in zfs volume activation
commit 815df2dd08ac4c7295135262e60d64fbb57b8f5c introduced a small issue
when activating linked clone volumes - the volname passed contains
basevol/subvol, which needs to be translated to subvol.

using the path method should be a robust way to get the actual path for
activation.

Found and tested by building the package as root (otherwise the zfs
regressiontests are skipped).

Reported-by: Thomas Lamprecht <t.lamprecht@proxmox.com>
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

PVE/Storage/ZFSPoolPlugin.pm
@@ -554,9 +554,10 @@ sub activate_volume {
     if ($format eq 'raw') {
 	$class->zfs_wait_for_zvol_link($scfg, $volname);
     } elsif ($format eq 'subvol') {
-	my $mounted = $class->zfs_get_properties($scfg, 'mounted', "$scfg->{pool}/$volname");
+	my ($path, undef, undef) = $class->path($scfg, $volname, $storeid);
+	my $mounted = $class->zfs_get_properties($scfg, 'mounted', "$path");
 	if ($mounted !~ m/^yes$/) {
-	    $class->zfs_request($scfg, undef, 'mount', "$scfg->{pool}/$volname");
+	    $class->zfs_request($scfg, undef, 'mount', "$path");
 	}
     }
 