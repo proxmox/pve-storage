f296ffc4e4d64b574c3001dc7cc6af3da1406441
Friedrich Weber <f.weber@proxmox.com>
2025-07-09 16:09:58 +0200

    fix #4997: lvm: create: disable autoactivation for new logical volumes
    When discovering a new volume group (VG), for example on boot, LVM
    triggers autoactivation. With the default settings, this activates all
    logical volumes (LVs) in the VG. Activating an LV creates a
    device-mapper device and a block device under /dev/mapper.
    
    This is not necessarily problematic for local LVM VGs, but it is
    problematic for VGs on top of a shared LUN used by multiple cluster
    nodes (accessed via e.g. iSCSI/Fibre Channel/direct-attached SAS).
    
    Concretely, in a cluster with a shared LVM VG where an LV is active on
    nodes 1 and 2, deleting the LV on node 1 will not clean up the
    device-mapper device on node 2. If an LV with the same name is
    recreated later, the leftover device-mapper device will cause
    activation of that LV on node 2 to fail with:
    
    > device-mapper: create ioctl on [...] failed: Device or resource busy
    
    Hence, certain combinations of guest removal (and thus LV removals)
    and node reboots can cause guest creation or VM live migration (which
    both entail LV activation) to fail with the above error message for
    certain VMIDs, see bug #4997 for more information [1].
    
    To avoid this issue in the future, disable autoactivation when
    creating new LVs using the `--setautoactivation` flag. With this
    setting, LVM autoactivation will not activate these LVs, and the
    storage stack will take care of activating/deactivating the LV (only)
    on the correct node when needed.
    
    This additionally fixes an issue with multipath on FC/SAS-attached
    LUNs where LVs would be activated too early after boot when multipath
    is not yet available, see [3] for more details and current workaround.
    
    The `--setautoactivation` flag was introduced with LVM 2.03.12 [2], so
    it is available since Bookworm/PVE 8, which ships 2.03.16. Nodes with
    older LVM versions ignore the flag and remove it on metadata updates,
    which is why PVE 8 could not use the flag reliably, since there may
    still be PVE 7 nodes in the cluster that reset it on metadata updates.
    
    The flag is only set for newly created LVs, so LVs created before this
    patch can still trigger #4997. To avoid this, users will be advised to
    run a script to disable autoactivation for existing LVs.
    
    [1] https://bugzilla.proxmox.com/show_bug.cgi?id=4997
    [2] https://gitlab.com/lvmteam/lvm2/-/blob/main/WHATS_NEW
    [3] https://pve.proxmox.com/mediawiki/index.php?title=Multipath&oldid=12039#FC/SAS-specific_configuration
    
    Signed-off-by: Friedrich Weber <f.weber@proxmox.com>
    Link: https://lore.proxmox.com/20250709141034.169726-2-f.weber@proxmox.com

--- src/PVE/Storage/LVMPlugin.pm
+++ src/PVE/Storage/LVMPlugin.pm
@@ -405,7 +405,18 @@ sub lvcreate {
         $size .= "k"; # default to kilobytes
     }
 
-    my $cmd = ['/sbin/lvcreate', '-aly', '-Wy', '--yes', '--size', $size, '--name', $name];
+    my $cmd = [
+        '/sbin/lvcreate',
+        '-aly',
+        '-Wy',
+        '--yes',
+        '--size',
+        $size,
+        '--name',
+        $name,
+        '--setautoactivation',
+        'n',
+    ];
     for my $tag (@$tags) {
         push @$cmd, '--addtag', $tag;
     }
