a7afad969d1e60afa3f7f3f430a4fd2c0b1d1bf4
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2025-07-29 13:53:16 +0200

    plugin: fix typo in rebase log message
    by directly printing the to-be-executed command, instead of copying it which is
    error-prone.
    
    Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>
    Reviewed-by: Fiona Ebner <f.ebner@proxmox.com>
    Link: https://lore.proxmox.com/20250729115320.579286-2-f.gruenbichler@proxmox.com
    [FE: use string concatenation rather than multi-argument print]
    Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

--- src/PVE/Storage/Plugin.pm
+++ src/PVE/Storage/Plugin.pm
@@ -1437,7 +1437,6 @@ sub volume_snapshot_delete {
             my $parentpath = $snapshots->{$parentsnap}->{file};
             print
                 "$volname: deleting snapshot '$snap' by rebasing '$childsnap' on top of '$parentsnap'\n";
-            print "running 'qemu-img rebase -b $parentpath -F qcow -f qcow2 $childpath'\n";
             $cmd = [
                 '/usr/bin/qemu-img',
                 'rebase',
@@ -1449,6 +1448,7 @@ sub volume_snapshot_delete {
                 'qcow2',
                 $childpath,
             ];
+            print "running '" . join(' ', $cmd->@*) . "'\n";
             eval { run_command($cmd) };
             if ($@) {
                 #in case of abort, the state of the snap is still clean, just a little bit bigger
