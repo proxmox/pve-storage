proxmox/pve-storage

commit e0eb405ea7043c91d357f6f0cad02cd7f16b6a40

zfspool: export: skip hardcoded warning about no-preserve-encryption flag
currently `zfs send --no-preserve-encryption` prints:
`WARNING: no-preserve-encryption flag set, sending dataset $dataset without encryption`
if the dataset is encrypted.

This patch skips the warning to not clutter the output, as the choice
is made deliberately.

[0] https://github.com/openzfs/zfs/blob/d8c08a1cea6428fa37b3a6585150b10dedfd79b8/lib/libzfs/libzfs_sendrecv.c#L616

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PVE/Storage/ZFSPoolPlugin.pm
@@ -824,7 +824,17 @@ sub volume_export {
     }
     push @$cmd, '--', "$scfg->{pool}/$dataset\@$snapshot";
 
-    run_command($cmd, output => $fd);
+    run_command(
+        $cmd,
+        output => $fd,
+        errfunc => sub {
+            my $line = shift;
+            if ($line !~ /^WARNING: no-preserve-encryption flag set, sending dataset/) {
+                print STDERR $line;
+                *STDERR->flush();
+            }
+        },
+    );
 
     return;
 }

commit 22cea6404b13fded68afee146b9e60383ae8eb2d

zfspool: export: fix regression with printing progress output
The progress from zfs send is printed to stderr, but the lines are
passed from run_command() to errfunc() without a trailing newline in
and so they would not show up in the task log right away, even with
the following call to flush. Use chomp() for future-proofing (e.g. if
there were an $errmsg, run_command() would call errfunc() with a
newline) and attach an explicit newline for each line passed to the
errfunc() to fix the issue.

Fixes: e0eb405 ("zfspool: export: skip hardcoded warning about no-preserve-encryption flag")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/20260723085132.29326-1-f.ebner@proxmox.com

src/PVE/Storage/ZFSPoolPlugin.pm
@@ -868,7 +868,8 @@ sub volume_export {
         errfunc => sub {
             my $line = shift;
             if ($line !~ /^WARNING: no-preserve-encryption flag set, sending dataset/) {
-                print STDERR $line;
+                chomp($line);
+                print STDERR "$line\n";
                 *STDERR->flush();
             }
         },